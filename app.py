6e2ea486c566bbdb7e0c02419b44dcad173a5f67
jegan94 <jegan94.r@gmail.com>
2025-04-30 16:25:32 +0530

    connect with mongo updated

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, jsonify, request
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
+from models import User
+from typing import List
 
 app = Flask(__name__)
+
+# app = FastAPI()
+
+MONGO_URI = "mongodb+srv://jeganrpalani:CBPw6ueTGchtJZDs@cluster0.y0lddgy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+client = MongoClient(MONGO_URI)
+db = client["chikku"]
+collection = db["user"]
 
 login_res = [
     {'status': '200', 'message': 'Login successfully', 'username': 'Test', 'mobile': 9876543210,
      'email': 'test@gmail.com'}
 ]
 
+
+@app.post("/users/")
+def create_user(user: User):
+    result = collection.insert_one(user.dict())
+    return {"id": str(result.inserted_id)}
 
 @app.route("/")
 def hello_world():
